c164c800d96c23686ea6f22b14815dcc194b1a0f
Summpot <summpot@outlook.com>
2024-07-02 15:07:59 +0800

    update kysely

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
