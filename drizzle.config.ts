aaabee75c054cca83ff1bea46ea6ed1747b11228
Summpot <summpot@outlook.com>
2024-07-02 19:25:42 +0800

    Update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,4 @@ export default defineConfig({
   dialect: "postgresql",
   out: "./drizzle/migrations",
   schema: "./drizzle/schema.ts",
-  migrations: {
-    table: "migrations_custom", // default `__drizzle_migrations`,
-    schema: "drizzle/schema.ts", // used in PostgreSQL only and default to `drizzle`
-  },
 });
