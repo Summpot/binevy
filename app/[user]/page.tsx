307c2b920195e275cd8739ca5501c6aae0661eb1
Summpot <summpot@outlook.com>
2024-07-03 15:25:56 +0800

    update

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -1,1 +1,10 @@
-export default function Profile() {}
+import { useParams } from "next/navigation";
+
+export default function Profile() {
+  const params = useParams<{ user: string; project: string }>();
+  return (
+    <>
+      {params.user}/{params.project}
+    </>
+  );
+}
